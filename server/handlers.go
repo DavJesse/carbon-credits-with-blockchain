e37593dff46d17894dc88cf9ee8a731978ff2fba
davodhiambo <jesseomolo@yahoo.com>
2024-07-30 03:19:48 +0300

    files: add server package and in it, handlers.go.

---
+++ server/handlers.go
@@ -0,0 +1,15 @@
+type CarbonCredit struct {
+	ID                 int    `json:"id"`
+	Issuer             string `json:"issuer"`
+	Amount             int    `json:"amount"`
+	VerificationStatus string `json:"verification_status"`
+}
+
+func issueCarbonCredit(w http.ResponseWriter, r *http.Request) {
+	// Issuing logic
+}
+
+func main() {
+	http.HandleFunc("/issue", issueCarbonCredit)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
