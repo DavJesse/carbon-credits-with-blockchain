8b2ef9fb149627f6195b9c00ffee44f759f9e29c
nyunja <poptiff@gmail.com>
2024-07-30 22:31:54 +0300

    feat: add css file server

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	database.InitDB()
-
+	http.Handle("frontend/site/assets/css", http.StripPrefix("frontend/site/assets/css", http.FileServer(http.Dir("/home/johnotieno0/hackathon/carbon-credits-with-blockchain/frontend/site/assets/css"))))
+	http.HandleFunc("/", user.HomeHandler)
 	http.HandleFunc("/register", user.RegisterUser)
 	http.HandleFunc("/login", user.LoginUser)
 	log.Fatal(http.ListenAndServe(":8080", nil))
