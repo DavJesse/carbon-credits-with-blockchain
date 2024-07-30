286dc6b2859becc31acf7ff1c8bcaa71d4333ada
Nyunja <poptiff@gmail.com>
2024-07-30 11:09:11 +0300

    refactor: move block code to blockchain/block.go

--- main.go
+++ main.go
@@ -1,55 +1,16 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	"carbomoney/blockchain"
 	"fmt"
-	"strconv"
-	"time"
 )
 
-type Block struct {
-	Index        int
-	Timestamp    int64
-	Data         string
-	PreviousHash string
-	Hash         string
-}
-
-type Blockchain struct {
-	chain []*Block
-}
-
-func newBlock(data, previousHash string, chain []*Block) *Block {
-	block := &Block{
-		Index:        len(chain) + 1,
-		Timestamp:    time.Now().Unix(),
-		Data:         data,
-		PreviousHash: previousHash,
-	}
-	block.Hash = calculateHash(block)
-	return block
-}
-
-func calculateHash(block *Block) string {
-	record := strconv.Itoa(block.Index) + strconv.Itoa(int(block.Timestamp)) + block.Data + block.PreviousHash
-	h := sha256.New()
-	h.Write([]byte(record))
-	hashed := h.Sum(nil)
-	return hex.EncodeToString(hashed)
-}
-
-func AddBlock(blockchain *Blockchain, data string, previousHash string) {
-	block := newBlock(data, previousHash, blockchain.chain) // Assuming NewBlock takes chain as an argument
-	blockchain.chain = append(blockchain.chain, block)
-}
-
 func main() {
-	blockchain := &Blockchain{}
-	AddBlock(blockchain, "Genesis Block", "0")
-	AddBlock(blockchain, "Block 1", blockchain.chain[0].Hash)
+	blockchain := blockchain.InitBlockChain()
+	blockchain.AddBlock("Genesis Block")
+	blockchain.AddBlock("Block 1")
 
-	for _, block := range blockchain.chain {
+	for _, block := range blockchain.Chain {
 		fmt.Printf("Block: %+v\n", block)
 	}
 }
